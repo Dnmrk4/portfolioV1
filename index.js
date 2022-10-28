542d9dd609f408f34047c90ef0a11ad9b37227de
Dnmrk4 <danmark.chemuren@gmail.com>
2022-10-28 06:13:53 +0300

    a fresh change on everything

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-    document.documentElement.classList.add('dark')
-    } else {
-    document.documentElement.classList.remove('dark')
-    }
+const btn = document.getElementById('menu-btn');
+const nav = document.getElementById('menu');
 
-    // Whenever the user explicitly chooses light mode
-    localStorage.theme = 'light'
+btn.addEventListener('click', () => {
+    btn.classList.toggle ('open');
+    btn.classList.toggle ('flex');
+    btn.classList.toggle ('hidden');
 
-    // Whenever the user explicitly chooses dark mode
-    localStorage.theme = 'dark'
-
-    // Whenever the user explicitly chooses to respect the OS preference
-    localStorage.removeItem('theme')
+})
